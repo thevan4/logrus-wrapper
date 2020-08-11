68328f9d95bedb00ca4db440c68d170fe7cabc79
Ivan Tikhonov Dmitrievich <4thevan@gmail.com>
2020-08-11 12:01:42 +0300

    init commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/thevan4/logrus-wrapper
+
+go 1.14
+
+require (
+	github.com/sirupsen/logrus v1.6.0
+	github.com/thevan4/logrus-graylog-http-hook v1.1.0
+)
---
+++ logger.go
@@ -0,0 +1,119 @@
+package logger
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log/syslog"
+	"net/http"
+	"os"
+	"path"
+	"runtime"
+
+	"github.com/sirupsen/logrus"
+	logrus_syslog "github.com/sirupsen/logrus/hooks/syslog"
+	graylogHttpHook "github.com/thevan4/logrus-graylog-http-hook"
+)
+
+// Logger ...
+type Logger struct {
+	Output           []string
+	Level            string
+	Formatter        string
+	SyslogTag        string
+	Graylog          *Graylog
+	LogEventLocation bool
+}
+
+// Graylog ...
+type Graylog struct {
+	Address    string
+	Retries    int
+	Extra      map[string]interface{}
+	HTTPClient *http.Client
+}
+
+// NewLogrusLogger create new logrus logger
+func NewLogrusLogger(logger *Logger) (*logrus.Logger, error) {
+	var err error
+	logrusLog := logrus.New()
+
+	err = logger.ApplyLoggerOut(logrusLog)
+	if err != nil {
+		return nil, err
+	}
+
+	logLevel, err := logrus.ParseLevel(logger.Level)
+	if err != nil {
+		return nil, err
+	}
+	logrusLog.SetLevel(logLevel)
+
+	err = logger.ApplyLogFormatter(logrusLog)
+	if err != nil {
+		return nil, err
+	}
+	return logrusLog, nil
+}
+
+// ApplyLoggerOut set log output
+func (logger *Logger) ApplyLoggerOut(logrusLog *logrus.Logger) error {
+	for _, logOutput := range logger.Output {
+		switch logOutput {
+		case "stdout":
+			logrusLog.SetOutput(os.Stdout)
+			continue
+		case "syslog":
+			syslog, err := logrus_syslog.NewSyslogHook("", "", syslog.LOG_INFO, logger.SyslogTag)
+			if err != nil {
+				return fmt.Errorf("can't create syslog hook: %v", err)
+			}
+			logrusLog.Hooks.Add(syslog)
+			logrusLog.SetOutput(ioutil.Discard)
+			continue
+		case "graylog":
+			graylogHook, err := graylogHttpHook.NewGraylogHook(logger.Graylog.Address, logger.Graylog.Retries, logger.Graylog.Extra, logger.Graylog.HTTPClient)
+			if err != nil {
+				return fmt.Errorf("can't create graylog hook: %s", err)
+			}
+			logrusLog.AddHook(graylogHook)
+			logrusLog.SetOutput(ioutil.Discard)
+			continue
+		default:
+			return fmt.Errorf("uknown log output type: %s", logOutput)
+		}
+	}
+	return nil
+}
+
+// ApplyLogFormatter set log format
+func (logger *Logger) ApplyLogFormatter(logrusLog *logrus.Logger) error {
+	switch logger.Formatter {
+	case "json":
+		jsonFormatter := &logrus.JSONFormatter{}
+		if logger.LogEventLocation {
+			logrusLog.SetReportCaller(true)
+			jsonFormatter.CallerPrettyfier = func(f *runtime.Frame) (string, string) {
+				filename := path.Base(f.File)
+				return fmt.Sprintf("%s()", f.Function), fmt.Sprintf("%s:%d", filename, f.Line)
+			}
+		}
+		logrusLog.SetFormatter(&logrus.JSONFormatter{})
+	case "text":
+		textFormatter := &logrus.TextFormatter{
+			TimestampFormat:  "2006-01-02 15:04:05",
+			FullTimestamp:    true,
+			QuoteEmptyFields: true,
+		}
+		if logger.LogEventLocation {
+			logrusLog.SetReportCaller(true)
+			textFormatter.CallerPrettyfier = func(f *runtime.Frame) (string, string) {
+				filename := path.Base(f.File)
+				return fmt.Sprintf("function=%s()", f.Function), fmt.Sprintf("file=%s:%d", filename, f.Line)
+			}
+		}
+		logrusLog.SetFormatter(textFormatter)
+	default:
+		return fmt.Errorf("uknown log format: %v", logger.Formatter)
+	}
+	return nil
+}
